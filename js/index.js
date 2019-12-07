8ef0a69d6afd51857688a62fa7d14753db408bc6
Georgi Lalev <glalev@gmail.com>
2019-12-07 23:44:42 +0200

    Available problems can be listed now

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
-const index = parseInt(process.argv[2], 10);
+const arg = process.argv[2];
+const index = parseInt(arg, 10);
+const fs = require('fs');
 // const problems = require('./problems');
 // const problem = problems['problem' + index];
+const logProblems = () => {
+  console.log('giting the problems');
+  fs.readdir('./problems/', (error, data) => {
+    if (error) return console.erorr(error);
+    const problems = data
+      .filter(file => file.toLowerCase().includes('problem'))
+      .map(file => parseInt(file.toLowerCase().split('.')[0].replace('problem', ''), 10))
+      .sort((a, b) => a - b);
+
+      console.log('problems available:', problems.join(', '));
+  });
+}
+
+if (arg === 'ls' || arg === 'list') {
+  return logProblems();
+}
+
 if (isNaN(index)) return console.log('Next time pass the problem as first paramether of type number!');
 
 try {
